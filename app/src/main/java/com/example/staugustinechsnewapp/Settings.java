708aa79c7a118e986ca3a1b7d767b124cd5f1d6e
Tiffany <tiffany.lee21@ycdsbk12.ca>
2021-02-11 11:17:14 -0500

    Songs Page

--- app/src/main/java/com/example/staugustinechsnewapp/Settings.java
+++ app/src/main/java/com/example/staugustinechsnewapp/Settings.java
@@ -2,21 +2,18 @@ package com.example.staugustinechsnewapp;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Profile#newInstance} factory method to
+ * Use the {@link Settings#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Profile extends Fragment {
+public class Settings extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +24,7 @@ public class Profile extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Profile() {
+    public Settings() {
         // Required empty public constructor
     }
 
@@ -37,11 +34,11 @@ public class Profile extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Profile.
+     * @return A new instance of fragment Settings.
      */
     // TODO: Rename and change types and number of parameters
-    public static Profile newInstance(String param1, String param2) {
-        Profile fragment = new Profile();
+    public static Settings newInstance(String param1, String param2) {
+        Settings fragment = new Settings();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,6 +59,6 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
