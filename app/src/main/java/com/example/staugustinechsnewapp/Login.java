41ffbf013e058b620987430c3449fcc6b66e47dc
BuildTools <unconfigured@null.spigotmc.org>
2021-06-17 16:05:43 -0400

    Started transporting old code to new app.

---
+++ app/src/main/java/com/example/staugustinechsnewapp/Login.java
@@ -0,0 +1,31 @@
+package com.example.staugustinechsnewapp;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login {
+
+
+}
--- app/src/main/java/com/example/staugustinechsnewapp/MainActivity.java
+++ app/src/main/java/com/example/staugustinechsnewapp/MainActivity.java
@@ -6,14 +6,35 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder().build();
+
+        FirebaseFirestore.getInstance().setFirestoreSettings(settings);
+
+        //FIREBASE MESSAGING SETTINGS
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -21,5 +42,14 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this,R.id.fragment);
         NavigationUI.setupWithNavController(bottomNavigationView,navController);
 
+    }
+
+    private void init(Bundle savedInstanceState) {
+        if(savedInstanceState == null) {
+            //THE USER ISN'T SIGNED INTO FIREBASE, GO TO LOGIN ACTIVITY
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
