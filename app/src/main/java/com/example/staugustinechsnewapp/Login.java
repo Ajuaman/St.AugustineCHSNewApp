eee328cb3983970cd0640a1ca80daedb8792fccf
Ajuaman <tate.chin24@gmail.com>
2021-12-18 17:33:12 -0500

    Added login page

--- app/src/main/java/com/example/staugustinechsnewapp/Login.java
+++ app/src/main/java/com/example/staugustinechsnewapp/Login.java
@@ -1,55 +1,29 @@
 package com.example.staugustinechsnewapp;
 
-import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
+public class Login extends Fragment {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
+    private SignInButton signInButton;
+    private GoogleSignInClient  mGoogleSignInClient;
+    private FirebaseAuth mAuth;
 
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
     }
-
-    public void createSignInIntent() {
-
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void finishSignInt() {
-
-    }
-    @Override
-    public void onClick(View v) {
-        //test
-        FirebaseFirestore.getInstance().collection("users")
-                .document(FirebaseAuth.getInstance().getUid());
-
-    }
-
 }
--- app/src/main/java/com/example/staugustinechsnewapp/Settings.java
+++ app/src/main/java/com/example/staugustinechsnewapp/Settings.java
@@ -10,34 +10,6 @@ import android.view.ViewGroup;
 
 public class Settings extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public Settings() {
-
-    }
-
-    public static Settings newInstance(String param1, String param2) {
-        Settings fragment = new Settings();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
